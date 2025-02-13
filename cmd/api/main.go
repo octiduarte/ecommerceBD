f73bbe70d4e382da6699c872ad5caa95de25ab02
octavio <octiduarte@hotmail.com>
2025-02-13 08:14:25 -0300

    sd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"simi/cmd/api/dependencies"
 	"simi/cmd/api/routes"
 	"simi/internal/utils/db"
@@ -11,6 +12,7 @@ import (
 )
 
 func main() {
+	// Obtener la conexión a la base de datos
 	database, err := db.GetDB()
 	if err != nil {
 		fmt.Println("Failed to connect to database with error: ", err)
@@ -20,6 +22,7 @@ func main() {
 
 	fmt.Println("Connected to database")
 
+	// Inicializar manejadores de dependencias
 	dm := dependencies.NewDependencyManager(database)
 
 	productsHandlers := dm.ProductsHandler()
@@ -27,23 +30,32 @@ func main() {
 	mainStoresHandler := dm.MainStoresHandler()
 	productsImageHandler := dm.ProductsImageHandler()
 
+	// Inicializar rutas
 	r := routes.InitRoutes(productsHandlers, storesHandler, mainStoresHandler, productsImageHandler)
 
-	// Configurar CORS para permitir solicitudes desde localhost:3000
+	// Configurar CORS para permitir solicitudes desde cualquier origen (ajústalo si es necesario)
 	corsHandler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
+		handlers.AllowedOrigins([]string{"*"}), // Permitir cualquier origen (cambiar si es necesario)
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 	)(r)
 
+	// Servir imágenes desde la carpeta interna
 	fs := http.FileServer(http.Dir("./internal/image"))
 	r.PathPrefix("/image/").Handler(http.StripPrefix("/image/", fs))
 
-	err = http.ListenAndServe(":8000", corsHandler)
+	// Obtener el puerto desde la variable de entorno (si no está, usar 8000 por defecto)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
+	fmt.Println("Server running on port:", port)
+
+	// Iniciar servidor
+	err = http.ListenAndServe(":"+port, corsHandler)
 	if err != nil {
 		fmt.Println("Error starting server with error: ", err)
 		panic(err)
 	}
-
 }
